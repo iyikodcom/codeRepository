9c025fa80b515f1823895b70ddbe0746bb36910a
iyikodcom <31039337+iyikodcom@users.noreply.github.com>
2020-08-24 16:30:38 +0300

    Update pdo.php

--- pdo.php
+++ pdo.php
@@ -1,5 +1,22 @@
 <?php
 
+//--veritabanı bağlantısı
+try {
+     $db = new PDO("mysql:host=localhost;dbname=veritabaniAdi;charset=utf8", "kullaniciAdi", "sifre");
+} catch ( PDOException $e ){
+     print $e->getMessage();
+}
 
+//--veritabanına veri eklme
+$insert = $db->prepare('INSERT INTO users (name, email) VALUES (?, ?)');
+$insert->execute(array('veri1','veri2'));
 
+//--veritabanındaki veriyi güncelleme
+
+//--veritabanındaki veriyi silme
+
+//--veritabanındaki verileri ekrana basma
+
+//--veritabanı bağlantısını sonlandırma
+$db = null;
 ?>
