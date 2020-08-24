fa0bef85933345f6f38ac41d56ecce9422dbff1c
iyikodcom <31039337+iyikodcom@users.noreply.github.com>
2020-08-24 17:05:31 +0300

    Update SQL.php

--- SQL.php
+++ SQL.php
@@ -1,4 +1,19 @@
 <?php
 
+**************************************************************************************************************************
+//--veri kaydetmek için kullanılan örnek sql sorgusu
+INSERT INTO musteri(ad, soyad, dtarih, sehir, cinsiyet, puan) VALUES ('Ali','Şahin','2000-10-12','Burdur','E',68)
+  
+**************************************************************************************************************************
+//--veri silmek için kullanılan örnek sql sorgusu
+DELETE FROM musteriler WHERE musterino = 4
+  
+**************************************************************************************************************************
+//--veri güncelleme için kullanılan örnek sql sorgusu
+UPDATE musteriler SET puan = 90 WHERE musterino = 3
+ 
+**************************************************************************************************************************
+//--ekrana tablodaki verileri yazdırmak için kullanılan örnek sql sorgusu
+SELECT * FROM musteri WHERE cinsiyet='K'
 
 ?>
