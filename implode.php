886ecfb05372ee6c3962306e23e2a91f5e460ac0
iyikodcom <31039337+iyikodcom@users.noreply.github.com>
2020-11-17 15:06:00 +0200

    Update implode.php

--- implode.php
+++ implode.php
@@ -1,3 +1,4 @@
+<?php
 //--php'de bulunan hazır fonksiyonlardan implode() fonksiyonunun kullanımı ile ilgili örnek kodlar;
 
 $array = array(1,2,3,4,5);
@@ -5,3 +6,4 @@ $implode = implode(',', $array);
 echo $implode;
 
 //--result: 1,2,3,4,5 -- (string)
+?>
