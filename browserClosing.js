51668e2c0e5f69efbcd77acf213290157077f25e
iyikodcom <31039337+iyikodcom@users.noreply.github.com>
2021-08-30 14:04:53 +0300

    Update browserClosing.js

--- browserClosing.js
+++ browserClosing.js
@@ -1,1 +1,31 @@
 //--aaa
+//-- +
+//-- tarayıcı kapandığında yada tarayıcının bulunduğu sekmesi kapandığında -- çalışacak fonksiyon
+function triggerFunction()
+{
+  $.ajax({
+    url:'',//-->servis
+    type:'post',//-->method
+    dataType:'json',//-->type
+    data: {...},//-->send data
+    success:function (response){
+
+      if(response.status == 'successful')
+      {	
+        console.log(response.message);
+
+      }
+      else
+      {
+        console.log(response.message);
+      }
+
+    },
+    error:function(jqXHR, textStatus, errorThrown){
+
+      console.log(textStatus, errorThrown);
+    }
+  });
+}
+
+window.onbeforeunload = triggerFunction;
