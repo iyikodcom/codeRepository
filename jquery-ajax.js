8f144e1d63dd7a50f31aa1bb5ce208b081ef9947
iyikodcom <31039337+iyikodcom@users.noreply.github.com>
2021-10-01 14:01:43 +0300

    Create jquery-ajax.js

---
+++ jquery-ajax.js
@@ -0,0 +1,23 @@
+$.ajax({
+		url:'***.php',
+		type:'post',
+		dataType:'json',
+		data:{...},
+		success:function (response){
+
+			if(response.status == 'successful')
+			{
+				alert(response.message);
+			}
+			else
+			{
+				alert(response.message);
+			}
+
+		},
+		error:function(jqXHR, textStatus, errorThrown){
+
+			console.log(textStatus, errorThrown);
+			
+		}
+	});
