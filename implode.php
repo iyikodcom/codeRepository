d001c41259bd3cca9f77a9e79f1d8071819b7920
iyikodcom <31039337+iyikodcom@users.noreply.github.com>
2026-03-24 19:25:35 +0300

    Delete implode.php

--- implode.php
+++
@@ -1,9 +0,0 @@
-<?php
-//--php'de bulunan hazır fonksiyonlardan implode() fonksiyonunun kullanımı ile ilgili örnek kodlar;
-
-$array = array(1,2,3,4,5);
-$implode = implode(',', $array);
-echo $implode;
-
-//--result: 1,2,3,4,5 -- (string)
-?>
